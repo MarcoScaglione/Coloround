655bd785ac1bf962d71da85c64f41c9360eb7ac8
Marco <marco.scaglione998@gmail.com>
2021-11-16 12:45:27 +0100

    Project created
    boh

---
+++ Coloround/Coloround/ColoroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColoroundApp.swift
+//  Coloround
+//
+//  Created by Marco Scaglione on 16/11/21.
+//
+
+import SwiftUI
+
+@main
+struct ColoroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
