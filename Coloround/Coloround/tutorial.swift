8a640afeab2a3bc27eb3e6c70a9b9ed8c82ebbc1
IlariaScognamiglio <92297795+IlariaScognamiglio@users.noreply.github.com>
2021-11-17 11:26:23 +0100

    tutorial

--- Coloround/Coloround/tutorial.swift
+++ Coloround/Coloround/tutorial.swift
@@ -26,29 +26,37 @@ struct ContentView: View {
 
 struct FirstScreen: View {
     var body: some View {
-        VStack {
-        Text("Welcome to")
-            .font(.title)
-            .fontWeight(.bold)
-            .padding(.top, 150.0)
-        Image("sfondop")
-            .resizable()
-            .aspectRatio(1.7777777778, contentMode: .fill)
-            .frame(height: 207)
-        Text ("This app is made to help Colorblind people to easily recognize and memorize colors using sounds")
-            .fontWeight(.bold)
-            .padding(.top, 50.0)
-        Text ("Swipe to the left to discover what you can do with it!")
-            .fontWeight(.bold)
-            .padding(.top, 50.0)
-        Spacer()
+        VStack(spacing: 30) {
+            Text("Welcome to")
+                .font(.title)
+                .fontWeight(.bold)
+                .padding(.top, 130.0)
+            Image("sfondop")
+                .resizable()
+                .aspectRatio(1.7777777778, contentMode: .fill)
+                .frame(height: 207)
+            Text ("This app is made to help Colorblind people to easily recognize and memorize colors using sounds")
+                .fontWeight(.bold)
+                .multilineTextAlignment(.center)
+                .padding()
+            
+            
+            Text ("Swipe to the left to discover what you can do with it!")
+                .fontWeight(.bold)
+                .multilineTextAlignment(.center)
+                .padding()
+            
+            Button(action: {}) {
+                Text("Skip the tutorial")
+           }
+            Spacer()
         }
     }
 }
 
 struct SecondScreen: View {
     var body: some View {
-        VStack{
+        VStack(spacing: 30){
             
         Text ("Discover colors of your favourite pics!")
             .foregroundColor(.black)
@@ -61,6 +69,7 @@ struct SecondScreen: View {
             
             HStack{
                 Image (systemName: "camera.viewfinder")
+                
                     
                     .padding(.leading, 50.0)
                 Text ("Choose your pic from your own album")
