e29450414cacd2d5a1d06233d56b2477ddcab66e
Marco <marco.scaglione998@gmail.com>
2021-11-20 22:13:57 +0100

    Changed the ImagePicker, it now should work with camera

---
+++ Coloround/Coloround/PhotoPickerView.swift
@@ -0,0 +1,31 @@
+//
+//  PhotoPickerView.swift
+//  Coloround
+//
+//  Created by Marco Scaglione on 20/11/21.
+//
+
+import UIKit
+import SwiftUI
+
+struct PhotoPickerView: UIViewControllerRepresentable {
+    
+    @Binding var selectedImage: UIImage?
+    @Environment(\.presentationMode) var isPresented
+    var sourceType: UIImagePickerController.SourceType
+        
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = self.sourceType
+        imagePicker.delegate = context.coordinator 
+        return imagePicker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+
+    }
+
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(picker: self)
+    }
+}
