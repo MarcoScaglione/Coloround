82493dc40ad9b6844aa5c8cd588a31abf40ebbce
silentiumec <48063907+silentiumec@users.noreply.github.com>
2021-11-23 11:48:40 +0100

    daniel changes

--- Coloround/Coloround/ColorPickerView/ColorPickerView.swift
+++ Coloround/Coloround/ColorPickerView/ColorPickerView.swift
@@ -7,35 +7,23 @@
 
 import SwiftUI
 
-// RS
 struct ColorPickerView: View {
-    let imageName: String = "monaLisa" // input parateter, remove value and delete file from the project
-<<<<<<< HEAD
-    
-    @StateObject var Settings = SettingsObject()
-    
-=======
-   
->>>>>>> parent of f75b9b3 (colorpickerview page with camera changes)
+    @State private var image: UIImage?
     @State private var color = Color.white
-    @State private var pickerPoint: CGPoint? = nil
-    @State private var Selected = 1// Ask why on iphone crashes if added
-    let myUnit = ToneOutputUnit()
-<<<<<<< HEAD
+    @State private var pickerPoint: CGPoint?
+    @State private var Selected = 1 // Ask why on iphone crashes if added
+    @State private var showingActionSheet = false
+    @State private var showingImagePicker = false
+    @State private var sourceType: UIImagePickerController.SourceType = .photoLibrary
     
+    private let myUnit = ToneOutputUnit()
     
-    
-=======
-    
-    
-    
->>>>>>> parent of f75b9b3 (colorpickerview page with camera changes)
-    func PlayFrequency(){
+    func PlayFrequency() {
         let rgbHz: (
             red: String,
             green: String,
             blue: String
-        ) = RGBColorFreq.hzFrom(S: Settings,
+        ) = RGBColor.hzFrom(
             r: color.components.red,
             g: color.components.green,
             b: color.components.blue
@@ -45,18 +33,18 @@ struct ColorPickerView: View {
             myUnit.setFrequency(freq: Double(rgbHz.red)!)
             myUnit.setToneVolume(vol: 0.5)
             myUnit.enableSpeaker()
-            myUnit.setToneTime(t: Settings.duration)
+            myUnit.setToneTime(t: 2)
         case 2:
             myUnit.setFrequency(freq: Double(rgbHz.green)!)
             myUnit.setToneVolume(vol: 0.5)
             myUnit.enableSpeaker()
-            myUnit.setToneTime(t: Settings.duration)
+            myUnit.setToneTime(t: 2)
             
         case 3:
             myUnit.setFrequency(freq: Double(rgbHz.blue)!)
             myUnit.setToneVolume(vol: 0.5)
             myUnit.enableSpeaker()
-            myUnit.setToneTime(t: Settings.duration)
+            myUnit.setToneTime(t: 2)
             
         default: break
         }
@@ -64,103 +52,159 @@ struct ColorPickerView: View {
 
     var body: some View {
         VStack {
-            ZStack {
-                GeometryReader { geometry in
-                    TappableImageView(width: geometry.size.width, name: imageName) { (color, point) in
-                        self.color = Color(color)
-                        pickerPoint = point
+            GeometryReader { geometry in
+                ZStack {
+                    if let image = self.image {
+                        // workaraund to get right imageView height
+                        let frame = geometry.frame(in: .named("ImageView"))
+                        TappableImageView(width: geometry.size.width,
+                                          height: geometry.size.height - abs(frame.origin.x),
+                                          image: image) { (color, point) in
+                            self.color = Color(color)
+                            pickerPoint = point
+                        }
+                    } else {
+                        HStack {
+                            Spacer()
+                            VStack(alignment: .center) {
+                                Spacer()
+                                Text("Please select an image")
+                                Spacer()
+                            }
+                            Spacer()
+                        }
                     }
-                
-                // Configure colors as you prefer
-                if let point = pickerPoint {
-                    Circle()
-                        .strokeBorder(Color.gray.opacity(0.3), lineWidth: 1)
-                        .background(Circle().fill(Color.white.opacity(0.6)))
-                        .frame(width: 16, height: 16)
-                        .position(x: point.x, y: point.y)
+                    
+                    // Configure colors as you prefer
+                    if let point = pickerPoint {
+                        Circle()
+                            .strokeBorder(Color.gray.opacity(0.3), lineWidth: 1)
+                            .background(Circle().fill(Color.white.opacity(0.6)))
+                            .frame(width: 16, height: 16)
+                            .position(x: point.x, y: point.y)
+                    }
                 }
+                .coordinateSpace(name: "ImageView")
+            }
+            
+            Spacer()
+            bottomView()
+            tabBarView()
+        }
+        .navigationBarTitleDisplayMode(.inline)
+        .navigationBarItems(trailing:
+            Button(action: {
+                showingActionSheet = true
+            }, label: {
+                Image(systemName: "photo")
+            })
+        )
+        .actionSheet(isPresented: $showingActionSheet) {
+            ActionSheet(title: Text("Image soure"), message: Text("Select where to pick an image"), buttons: [
+                .default(Text("Camera")) {
+                    sourceType = .camera
+                    showingImagePicker = true
+                },
+                .default(Text("Gallery")) {
+                    sourceType = .photoLibrary
+                    showingImagePicker = true
+                },
+                .cancel()
+            ])
+        }
+        .sheet(isPresented: self.$showingImagePicker) {
+            PhotoPickerView(selectedImage: $image, sourceType: self.sourceType)
+        }
+    }
+    
+    @ViewBuilder private func bottomView() -> some View {
+        HStack(spacing: 8) {
+            Rectangle()
+                .fill(color)
+                .cornerRadius(8)
+                .frame(width: 100, height: 100)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(Color.gray, lineWidth: 1)
+                )
+                .padding()
+            HStack {
+                VStack {
+                    Text("R")
+                        .foregroundColor(Color.red)
+                    Text("G")
+                        .foregroundColor(Color.green)
+                    Text("B")
+                        .foregroundColor(Color.blue)
+                }
+                VStack {
+                    Text(Int(color.components.red).description)
+                    Text(Int(color.components.green).description)
+                    Text(Int(color.components.blue).description)
                 }
             }
             
             Spacer()
             
-            HStack(spacing: 8) {
-                Rectangle()
-                    .fill(color)
-                    .cornerRadius(8)
-                    .frame(width: 100, height: 100)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 8)
-                            .stroke(Color.gray, lineWidth: 1)
+            HStack {
+                VStack {
+                    let rgbHz: (
+                        red: String,
+                        green: String,
+                        blue: String
+                    ) = RGBColor.hzFrom(
+                        r: color.components.red,
+                        g: color.components.green,
+                        b: color.components.blue
                     )
-                    .padding()
-                HStack {
-                    VStack {
-                        Text("R")
-                            .foregroundColor(Color.red)
-                        Text("G")
-                            .foregroundColor(Color.green)
-                        Text("B")
-                            .foregroundColor(Color.blue)
-                    }
-                    VStack {
-                        Text(Int(color.components.red).description)
-                        Text(Int(color.components.green).description)
-                        Text(Int(color.components.blue).description)
-                    }
+                    Text(rgbHz.red)
+                    Text(rgbHz.green)
+                    Text(rgbHz.blue)
                 }
-                
-                Spacer()
-                
-                HStack {
-                    VStack {
-                        let rgbHz: (
-                            red: String,
-                            green: String,
-                            blue: String
-<<<<<<< HEAD
-                        ) = RGBColorFreq.hzFrom(S: Settings,
-=======
-                        ) = RGBColor.hzFrom(
->>>>>>> parent of f75b9b3 (colorpickerview page with camera changes)
-                            r: color.components.red,
-                            g: color.components.green,
-                            b: color.components.blue
-                        )
-                        Text(rgbHz.red)
-                        Text(rgbHz.green)
-                        Text(rgbHz.blue)
-                    }
-                    VStack {
-                        Text("Hz")
-                        Text("Hz")
-                        Text("Hz")
-                    }
+                VStack {
+                    Text("Hz")
+                    Text("Hz")
+                    Text("Hz")
                 }
-                
-                Spacer()
-                
-                Button(action: PlayFrequency) {
-                    
-                    VStack(spacing : 3){
-                        
-                        Image(systemName: "play.fill")
+            }
+            
+            Spacer()
+            
+            Button(action: PlayFrequency) {
+                VStack(spacing : 3){
+                    Image(systemName: "play.fill")
                         .dynamicTypeSize(.accessibility3)
-                        Text("Play")
-                    }
-                    .padding(.horizontal, 5.0)
+                    Text("Play")
                 }
-                
+                .padding(.horizontal, 5.0)
+            }
+            
+            Spacer()
+        }
+    }
+    
+    @ViewBuilder private func tabBarView() -> some View {
+        VStack(spacing: 12) {
+            Rectangle()
+                .foregroundColor(.gray)
+                .frame(height: 1)
+                .frame(maxWidth: .infinity)
+            HStack {
+                Spacer()
+                Button("R", action: {
+                  Selected = 1
+                }).foregroundColor(Selected == 1 ? .blue : .gray)
+                Spacer()
+                Button("G", action: {
+                  Selected = 2
+                }).foregroundColor(Selected == 2 ? .blue : .gray)
+                Spacer()
+                Button("B", action: {
+                  Selected = 3
+                }).foregroundColor(Selected == 3 ? .blue : .gray)
                 Spacer()
             }
         }
-        .navigationBarItems(trailing:
-            Button(action: {
-                print("favorites button pressed")
-            }) {
-                Image("suit.heart")
-            }
-        )
     }
 }
 
--- Coloround/Coloround/ColorPickerView/TappableImageView.swift
+++ Coloround/Coloround/ColorPickerView/TappableImageView.swift
@@ -6,15 +6,15 @@
 //
 
 import SwiftUI
-
-// RS
+import UIKit
 
 struct TappableImageView: UIViewRepresentable {
-    var width: CGFloat
-    var name: String
+    let width: CGFloat
+    let height: CGFloat
+    let image: UIImage
     var tappedCallback: ((UIColor, CGPoint) -> Void)
 
-    class Coordinator: NSObject {
+    class Coordinator: NSObject, UIGestureRecognizerDelegate {
         var tappedCallback: ((UIColor, CGPoint) -> Void)
         
         init(tappedCallback: @escaping ((UIColor, CGPoint) -> Void)) {
@@ -22,12 +22,42 @@ struct TappableImageView: UIViewRepresentable {
         }
         
         @objc func tapped(gesture: UITapGestureRecognizer) {
-            let point = gesture.location(in: gesture.view)
-            
-            if let view = gesture.view,
-               let color = getPixelColorAtPoint(point: point, sourceView: view) {
-                self.tappedCallback(color, point)
+            guard let view = gesture.view as? UIImageView else {
+                return
             }
+            
+            let point = gesture.location(in: view)
+            let imageRect = imageRect(from: view)
+            
+            if imageRect.contains(point) {
+                if let color = getPixelColorAtPoint(point: point, sourceView: view) {
+                    self.tappedCallback(color, point)
+                }
+            }
+        }
+        
+        private func imageRect(from imageView: UIImageView) -> CGRect {
+            let imageViewSize = imageView.frame.size
+            let imgSize = imageView.image?.size
+            
+            guard let imageSize = imgSize else {
+                return CGRect.zero
+            }
+            
+            let scaleWidth = imageViewSize.width / imageSize.width
+            let scaleHeight = imageViewSize.height / imageSize.height
+            let aspect = fmin(scaleWidth, scaleHeight)
+            
+            var imageRect = CGRect(x: 0, y: 0, width: imageSize.width * aspect, height: imageSize.height * aspect)
+            // Center image
+            imageRect.origin.x = (imageViewSize.width - imageRect.size.width) / 2
+            imageRect.origin.y = (imageViewSize.height - imageRect.size.height) / 2
+            
+            // Add imageView offset
+            imageRect.origin.x += imageView.frame.origin.x
+            imageRect.origin.y += imageView.frame.origin.y
+            
+            return imageRect
         }
         
         private func getPixelColorAtPoint(point:CGPoint, sourceView: UIView) -> UIColor? {
@@ -58,8 +88,9 @@ struct TappableImageView: UIViewRepresentable {
     
     func makeUIView(context: Context) -> UIImageView {
         let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
         imageView.isUserInteractionEnabled = true
-        imageView.image = UIImage(named: name)?.resizedImage(newWidth: UIScreen.main.bounds.width)
+        imageView.image = image.resizedImage(width: width, height: height)
         let gesture = UITapGestureRecognizer(target: context.coordinator,
                                              action: #selector(Coordinator.tapped))
         imageView.addGestureRecognizer(gesture)
@@ -67,13 +98,27 @@ struct TappableImageView: UIViewRepresentable {
     }
     
     func updateUIView(_ uiView: UIImageView, context: Context) {
+        uiView.image = image.resizedImage(width: width, height: height)
     }
 }
 
 private extension UIImage {
-    func resizedImage(newWidth: CGFloat) -> UIImage? {
-        let scale = newWidth / size.width
-        let newHeight = size.height * scale
+    
+    // resize to fit input size and keep aspect
+    func resizedImage(width: CGFloat, height: CGFloat) -> UIImage? {
+        let newWidth: CGFloat
+        let newHeight: CGFloat
+        
+        if size.width > size.height {
+            let scale = width / size.width
+            newHeight = size.height * scale
+            newWidth = width
+        } else {
+            let scale = height / size.height
+            newWidth = size.width * scale
+            newHeight = height
+        }
+
         UIGraphicsBeginImageContext(.init(width: newWidth, height: newHeight))
         self.draw(in: .init(x: 0, y: 0, width: newWidth, height: newHeight))
         let newImage = UIGraphicsGetImageFromCurrentImageContext()
